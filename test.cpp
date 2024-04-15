e84fda6e12238cce5d18f6ab60710a9bbafb4afa
Jake Hosking <hosking@usc.edu>
2024-04-14 18:37:50 -0700

    Test code works, yippie!

--- test.cpp
+++ test.cpp
@@ -8,23 +8,26 @@
 #define LENGTH 10
 //256
 
-static const char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+static const char base64chars[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                   'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                   'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                   'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
 
 void KAT_Blake2(uint8_t *input, uint8_t *hash)
 {
     int i, j;
-    printf("Running Blake2 :)");
+    printf("Running Blake2 :)\n\n");
 
     for (i = 0; i < LENGTH; ++i)
     {
-        printf("NEW WORD\n");
-        for (j = 0; j < i; ++j) {
-            printf("Input Index %i is - %i\n", j, input[j]);
-        }
+        // printf("NEW WORD\n");
+        // for (j = 0; j < i; ++j) {
+        //     printf("Input Index %i is - %i\n", j, input[j]);
+        // }
         blake2b(hash, BLAKE2B_OUTBYTES, input, i, NULL, 0);
         printf("\t{\n\t\t");
 
-        for (j = 0; j < BLAKE2B_OUTBYTES; j += 3)
+        for (j = 0; j < BLAKE2B_OUTBYTES - 3; j += 3)
         {
             // Encode the first character
             printf("%c", base64chars[hash[j] >> 2]);
@@ -34,16 +37,14 @@ void KAT_Blake2(uint8_t *input, uint8_t *hash)
             printf("%c", base64chars[((hash[j + 1] & 0x0f) << 2) | ((hash[j + 2] & 0xc0) >> 6)]);
             // Encode the fourth character
             printf("%c", base64chars[hash[j + 2] & 0x3f]);
-        }
 
-        // Print padding if necessary
-        if (i < LENGTH - 1 || (BLAKE2B_OUTBYTES % 3) != 0)
-        {
-            int padding = ((BLAKE2B_OUTBYTES % 3) == 1) ? 2 : (((BLAKE2B_OUTBYTES % 3) == 2) ? 1 : 0);
-            for (int p = 0; p < padding; p++)
-            {
-                printf("=");
-            }
+            // printf("\tAdded indexes %i to %i\n", j, j + 2);
+        }
+        if (j >= 63) {
+            printf("%c", base64chars[hash[j] >> 2]);
+            printf("%c", base64chars[(hash[j] & 0x03) << 4]);
+            printf("=");
+            printf("=");
         }
 
         printf("\n\t},\n");
@@ -104,7 +105,7 @@ int main() {
       input[i] = i;
 
     for (i = 0; i < sizeof(key); ++i)
-      key[i] = i;
+      key[i] = 1;
 
     KAT_Blake2(input, hash);
     KAT_Blake2_Keyed(key, input, hash);
